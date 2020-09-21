aacf04a10df48ca3d04a4c24293ffc1607d67af6
randyfortier <randy.fortier@gmail.com>
2020-09-21 17:02:47 -0400

    Added last dart-only coding examples

--- 02_Dart/dart2.dart
+++ 02_Dart/dart2.dart
@@ -1,10 +1,68 @@
-class Course {
-  String _code;
-  String _name;
+import 'dart:async';
 
-  Course(this._code, this._name, int age);
+// class Course {
+//   String _code;
+//   String _name;
 
-  String toString() {
-    return 'Course($_code)';
-  }
+//   Course(int age, {this._code, this._name}) {
+//     // do something with age?
+//   }
+
+//   String toString() {
+//     return 'Course($_code)';
+//   }
+// }
+
+// var c = Course(21, _code = 'CSCI4100U', _name = 'Mobile Devices');
+
+// class Online {
+//   String url;
+// }
+
+// class Battler {
+//   int hp;
+//   int defense;
+//   int attack;
+// }
+
+// class Healer {
+//   int healPower;
+
+//   void heal(Battler b) {
+//     b.hp += 10;
+//   }
+// }
+
+// class Sprite {
+//   // stuff related to sprites
+// }
+
+// class Player with Battler, Sprite, Healer {
+//   String name;
+//   int level;
+//   int xp;
+// }
+
+Future<String> longTermOperation(int numSeconds, String message) async {
+  print('$message: operation started');
+  String result = await Future.delayed(Duration(seconds: numSeconds), () {
+    print('$message: after the delay');
+    return message;
+  });
+  print('$message: after the delayed');
+  return result;
+}
+
+// main() {
+//   longTermOperation(5, 'hello from async').then((result) {
+//     print(result);
+//   });
+//   print('after longTermOperation');
+// }
+
+// async/await version, running it twice:
+main() async {
+  String s = await longTermOperation(5, 'hello from async');
+  String s1 = await longTermOperation(5, 'goodbye from async');
+  print('after longTermOperation: ${s}, ${s1}');
 }
