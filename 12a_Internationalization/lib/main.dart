52a2e663f04811b4e77977faf4f52473fbed9fe1
ExperimentsInCS <randy.fortier@gmail.com>
2020-12-07 17:22:04 -0500

    Added i18n example

--- 12a_Internationalization/lib/main.dart
+++ 12a_Internationalization/lib/main.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:flutter_i18n/flutter_i18n_delegate.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+Future main() async {
+  final FlutterI18nDelegate flutterI18nDelegate = FlutterI18nDelegate(
+    translationLoader: FileTranslationLoader(
+        useCountryCode: false,
+        fallbackFile: 'en',
+        basePath: 'assets/flutter_i18n'),
+  );
+  WidgetsFlutterBinding.ensureInitialized();
+  await flutterI18nDelegate.load(null);
+  runApp(MyApp(flutterI18nDelegate));
 }
 
 class MyApp extends StatelessWidget {
+  final FlutterI18nDelegate flutterI18nDelegate;
+
+  MyApp(this.flutterI18nDelegate);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,6 +30,11 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MainPage(title: 'Internationalization'),
+      localizationsDelegates: [
+        flutterI18nDelegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
     );
   }
 }
@@ -28,13 +49,87 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  String _username;
+  String _password;
+  bool _validLogin;
+  int _numAttempts = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('EN'),
+            onPressed: () async {
+              print('Switching to english');
+              Locale newLocale = Locale('en');
+              await FlutterI18n.refresh(context, newLocale);
+              setState(() {});
+            },
+          ),
+          FlatButton(
+            child: Text('PT'),
+            onPressed: () async {
+              print('Switching to portugese');
+              Locale newLocale = Locale('pt');
+              await FlutterI18n.refresh(context, newLocale);
+              setState(() {});
+            },
+          ),
+        ],
       ),
-      body: Center(),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text(_validLogin == null
+                ? ''
+                : _validLogin
+                    ? FlutterI18n.translate(context, "login.success",
+                        translationParams: {"username": _username})
+                    : FlutterI18n.translate(context, "login.failure")),
+          ),
+          ListTile(
+            title: I18nPlural("login.num_attempts", _numAttempts),
+          ),
+          ListTile(
+            leading: Text(FlutterI18n.translate(context, "login.username")),
+            title: TextField(
+              controller: TextEditingController(text: _username),
+              onChanged: (newValue) {
+                _username = newValue;
+              },
+            ),
+          ),
+          ListTile(
+            leading: Text(FlutterI18n.translate(context, "login.password")),
+            title: TextField(
+              controller: TextEditingController(text: _password),
+              onChanged: (newValue) {
+                _password = newValue;
+              },
+            ),
+          ),
+          ListTile(
+            title: FlatButton(
+              child: Text(FlutterI18n.translate(context, "login.login")),
+              onPressed: () {
+                if (_username == 'admin' && _password == '12345') {
+                  setState(() {
+                    _validLogin = true;
+                  });
+                } else {
+                  _validLogin = false;
+                  _numAttempts++;
+                  setState(() {
+                  });
+                }
+              },
+            ),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         tooltip: 'Increment',
